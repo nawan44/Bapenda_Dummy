81b91b77753e39fcd31e34ac4683b982ccab1d60
nawan44 <rachmat.d.gunawan@gmail.com>
2023-05-10 14:24:08 +0700

    pie status device, data dummy merchant

--- src/routes/Dashboard/Pie Chart/index.js
+++ src/routes/Dashboard/Pie Chart/index.js
@@ -6,11 +6,14 @@ import * as moment from "moment";
 import PiePendapatan from "./piePendapatan";
 import PieStatus from "./pieStatus";
 import earnsbycatData from "../../../components/DataDummy/earnsbycat.json";
+import merchantsData from "../../../components/DataDummy/merchants.json";
 
 // import { latestTransaction1 } from "../../../components/DataDummy";
 
 function PieChart() {
   const [getEarnByCat, setGetEarnByCat] = useState(earnsbycatData);
+  const [listDevice, setListDevice] = useState(merchantsData);
+
   // const [earnByCat, setEarnByCat] = useState(0);
   const sThisMonth = moment().startOf("year").format("YYYY-MM-DD HH:mm:ss");
   const eThisMonth = moment().endOf("year").format("YYYY-MM-DD HH:mm:ss");
@@ -45,16 +48,52 @@ function PieChart() {
   //   const ajson = await response.json();
   //   setGetEarnByCat(ajson.Records);
   // };
-  const changeData =
+
+  const statusFilter =
+    listDevice &&
+    listDevice.map((row) => ({
+      // nik: row[3].stringValue,
+
+      type: row[7].stringValue,
+      name: row[9].stringValue,
+    }));
+
+  const changeDataKategori =
     getEarnByCat &&
     getEarnByCat.map((row) => ({
       value: Number(row[1].stringValue),
       name: row[0].stringValue,
     }));
-  const COLORS = ["#0088FE", "#00C49F", "#FFBB28"];
-  const data = changeData?.map((item, i) => ({ ...item, color: COLORS[i] }));
+  const COLORS = ["#0088FE", "#5B2C6F", "#a0acb8"];
+  const dataKategori = changeDataKategori?.map((item, i) => ({
+    ...item,
+    color: COLORS[i],
+  }));
+  let changeDataStatusDevice =
+    statusFilter &&
+    statusFilter.length &&
+    Object.values(
+      statusFilter.reduce((a, { name }) => {
+        let key = `${name}`;
+        a[key] = a[key] || { name, value: 0 };
+        a[key].value++;
+        return a;
+      }, {})
+    );
 
-  console.log("data", data);
+  var warna = [
+    { nama: "green", hex: "#008000" },
+    { nama: "orange", hex: "#FFA500" },
+    { nama: "red", hex: "#FF0000" },
+  ];
+
+  changeDataStatusDevice.forEach(
+    (e) => (e.hex = warna.find((d) => d.nama === e.name)?.hex || "")
+  );
+  console.log("changeDataStatusDevice ", changeDataStatusDevice);
+
+  console.log("warna ", warna);
+
   return (
     <>
       <Col span={12}>
@@ -68,7 +107,7 @@ function PieChart() {
             <h3 style={{ fontWeight: "bold" }}>Pendapatan Per Kategori</h3>
           }
         >
-          {data.map((item) => (
+          {dataKategori.map((item) => (
             <span style={{ margin: "0px 30px" }}>
               <Space size="large">
                 <Badge.Ribbon text={item.name} color={item.color} />
@@ -76,12 +115,36 @@ function PieChart() {
             </span>
           ))}
 
-          <PiePendapatan data={data} />
+          <PiePendapatan data={dataKategori} />
         </Card>
       </Col>
       <Col span={12}>
-        <Card className="gx-card" title="Status Device">
+        {/* <Card className="gx-card" title="Status Device">
           <PieStatus getEarnByCat={getEarnByCat} />
+        </Card> */}
+
+        <Card
+          style={{
+            margin: 0,
+            padding: 0,
+            textAlign: "center",
+          }}
+          title={<h3 style={{ fontWeight: "bold" }}>Status Device</h3>}
+        >
+          {changeDataStatusDevice.map((item) => (
+            <span
+              style={{
+                margin: "0px 40px",
+                width: "100%",
+              }}
+            >
+              <Space size="large">
+                <Badge.Ribbon text={item.name} color={item.hex} />
+              </Space>
+            </span>
+          ))}
+
+          <PieStatus data={changeDataStatusDevice} />
         </Card>
       </Col>
     </>
