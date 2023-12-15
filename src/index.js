e56fed2286bdea0d5d5898f96aae048ac6f3a8f0
nawan44 <rachmat.d.gunawan@gmail.com>
2023-12-15 10:39:41 +0700

    new2

--- src/index.js
+++ src/index.js
@@ -3,9 +3,9 @@ import ReactDOM from "react-dom";
 
 import NextApp from "./NextApp";
 import * as serviceWorker from "./registerServiceWorker";
-import "react-app-polyfill/ie11";
-import "react-app-polyfill/stable";
-import "resize-observer-polyfill/dist/ResizeObserver.global";
+// import "react-app-polyfill/ie11";
+// import "react-app-polyfill/stable";
+// import "resize-observer-polyfill/dist/ResizeObserver.global";
 
 ReactDOM.render(<NextApp />, document.getElementById("root"));
 
